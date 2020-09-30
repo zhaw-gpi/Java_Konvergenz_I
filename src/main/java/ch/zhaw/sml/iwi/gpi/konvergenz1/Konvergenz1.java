7ed54b06529c606bb700e01ad2ba4785972f5213
Heinrich Peter (heip) <heip@zhaw.ch>
2020-09-30 10:15:12 +0200

    Test Commit

--- src/main/java/ch/zhaw/sml/iwi/gpi/konvergenz1/Konvergenz1.java
+++ src/main/java/ch/zhaw/sml/iwi/gpi/konvergenz1/Konvergenz1.java
@@ -1,12 +1,24 @@
 package ch.zhaw.sml.iwi.gpi.konvergenz1;
 
 /**
- * Konvergenz1
+ *  Agenda
+ *  ======
+ * 
+ *  1) Git und Versionsverwaltung
+ *  2) Java, Sourcen und Maven
+ *  3) Klassen und Instanzen
+ *  4) Variablen und deren Scope
+ *  5) Composition over Inheritance 
+ *  6) Interfaces
+ *  7) Datentstrukturen (List und Map)
+ *  8) Exceptions
+ *  9) Debuggen
+ * 
  */
 public class Konvergenz1 {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");  
+    System.out.println("Hello, World!!");  
   }
   
 }
