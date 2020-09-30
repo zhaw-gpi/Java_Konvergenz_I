83535779a68de612bc472d5e03ed703621e65e9f
Heinrich Peter (heip) <heip@zhaw.ch>
2020-09-30 09:52:08 +0200

    Initial Commit

---
+++ src/main/java/ch/zhaw/sml/iwi/gpi/konvergenz1/Konvergenz1.java
@@ -0,0 +1,12 @@
+package ch.zhaw.sml.iwi.gpi.konvergenz1;
+
+/**
+ * Konvergenz1
+ */
+public class Konvergenz1 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");  
+  }
+  
+}
